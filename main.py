e880e655c65d6f6c6536a298805b2838ea53ca03
netprtony <huynhvikhang6a13@gmail.com>
2024-10-21 15:55:02 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+from pyspark.sql import SparkSession
+
+# Khởi tạo Spark session
+spark = SparkSession.builder \
+    .appName("Test Spark") \
+    .getOrCreate()
+
+print("Spark session created successfully!")
+
+# Tắt Spark session
+spark.stop()
